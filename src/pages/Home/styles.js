68428bfe97cfc490b73fb738a8ced8f96a44fb56
Roberto jose <robertofernandes199@hotmail.com>
2021-07-31 21:35:42 -0300

    busca-de-restaurantes
    commit 1º da api busca de restaurantes !

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row; 
+`;
+
+export const Container = styled.aside`
+    background-color: ${(props) => props.theme.colors.background};
+    width: 360px;
+    height: 100vh;
+    overflow-y: auto; 
+`;
+
+export const Search = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color: #ffffff;
+    padding: 18px;
+`;
+
+export const Logo = styled.img`
+    margin-bottom: 15px;
+
+`;
+
+export const Map = styled.div`
+    background-color: red;
+    width: 500px;
+`;
