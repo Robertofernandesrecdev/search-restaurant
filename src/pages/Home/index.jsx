4a1f9ae9b8cbbf6f439da1efc29582bb1e9083dc
Roberto jose <robertofernandes199@hotmail.com>
2021-08-02 22:33:46 -0300

    continuação...
    continuação do projeto busca de restaurantes.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,25 @@
 import React, {useState} from 'react';
+import Slider from "react-slick";
 import TextField, {Input} from '@material/react-text-field';
 import MaterialIcon from '@material/react-material-icon';
 import logo from '../../assets/logo.svg';
-import { Container, Search, Logo, Wrapper, Map } from './styles';
+import restaurante from '../../assets/restaurante-fake.png';
+import { Card } from '../../components';
+import { Container, Carousel, Search, Logo, Wrapper, Map, CarouselTitle } from './styles';
 
 const Home = () => {
     const [inputValue, setInputValue] = useState('');
+    
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        adaptiveHeight: true,
+      };
+
     return (
-
-
         <Wrapper>
         <Container>
             <Search>
@@ -24,6 +35,17 @@ const Home = () => {
                         onChange={(e) => setInputValue(e.target.value)} 
                     />
                 </TextField>
+                <CarouselTitle>Na sua Área</CarouselTitle>
+                <Carousel {...settings}>
+                    <Card photo={restaurante} title="Nome aqui!"/>
+                    <Card photo={restaurante} title="Nome aqui!"/>                    <Card photo={restaurante} />
+                    <Card photo={restaurante} title="Nome aqui!"/>                    <Card photo={restaurante} />
+                    <Card photo={restaurante} title="Nome aqui!"/> 
+                    <Card photo={restaurante} title="Nome aqui!"/>
+                    <Card photo={restaurante} title="Nome aqui!"/>
+                    <Card photo={restaurante} title="Nome aqui!"/>
+                    <Card photo={restaurante} title="Nome aqui!"/>                   
+                </Carousel>
             </Search> 
         </Container>
         <Map/>
